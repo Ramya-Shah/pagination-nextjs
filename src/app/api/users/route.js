b543d23144849aef3d2997b646394446f0813cdc
Ramya-Shah <176913971+Ramya-Clg@users.noreply.github.com>
2025-09-20 11:41:26 +0530

    cache

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,26 +1,29 @@
 import fs from "fs";
 import path from "path";
 import Papa from "papaparse";
-
+let cachedData = null;
 export async function GET(req) {
-  const filePath = path.join(process.cwd(), "public", "users.csv");
-  const file = fs.readFileSync(filePath, "utf8");
+    if (!cachedData) {
+        const filePath = path.join(process.cwd(), "public", "users.csv");
+        const file = fs.readFileSync(filePath, "utf8");
+        const { data } = Papa.parse(file, { header: true });
+        cachedData = data; 
+    }
+    return cachedData;
+}
 
-  const { data } = Papa.parse(file, { header: true });
+const { searchParams } = new URL(req.url);
+const page = parseInt(searchParams.get("page") || "1");
+const limit = parseInt(searchParams.get("limit") || "20");
+const start = (page - 1) * limit;
+const end = start + limit;
 
-  const { searchParams } = new URL(req.url);
-  const page = parseInt(searchParams.get("page") || "1");
-  const limit = parseInt(searchParams.get("limit") || "20");
-  const start = (page - 1) * limit;
-  const end = start + limit;
-
-  return new Response(JSON.stringify({
+return new Response(JSON.stringify({
     users: data.slice(start, end),
     total: data.length,
     page,
     pages: Math.ceil(data.length / limit)
-  }), {
+}), {
     headers: { "Content-Type": "application/json" },
-  });
+});
 }
-    
